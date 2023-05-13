6f3f506e01158a3fb73e6bf37f0006e60dab11c3
Jagos Perovic <83599132+Jagac@users.noreply.github.com>
2023-05-13 12:49:12 -0700

    cleaned up

--- text_modeling.py
+++ text_modeling.py
@@ -2,23 +2,27 @@ from transformers import AutoTokenizer, AutoModelForSequenceClassification
 import torch
 import pandas as pd
 import swifter
-#from utils import append_tables
+import optuna
+import gensim.corpora as corpora
+from utils import append_tables
 from sqlalchemy import create_engine
 from bertopic import BERTopic
 from sentence_transformers import SentenceTransformer
-import gensim.corpora as corpora
 from gensim.models.coherencemodel import CoherenceModel
-import optuna
 from umap import UMAP
 from hdbscan import HDBSCAN
 
-
 def load_data():
-    #append_tables()
+    """Loads a combined table of both sources
+
+    Returns:
+        pd.DataFrame: dataframe containing merged data
+    """
+    append_tables()
     conn_string = 'postgresql://postgres:123@127.0.0.1/postgres'
     db = create_engine(conn_string)
     conn = db.connect()
-    df = pd.read_sql_query("SELECT * FROM youtube_data;", con=conn)
+    df = pd.read_sql_query("SELECT * FROM combined_data;", con=conn)
     
     return df
     
@@ -33,16 +37,26 @@ def sentiment_label(text):
         int : 0 -> neg, 1 -> neut, 2 -> pos
     """
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(device)
     tokenizer = AutoTokenizer.from_pretrained('cardiffnlp/twitter-roberta-base-sentiment-latest')
     model = AutoModelForSequenceClassification.from_pretrained('cardiffnlp/twitter-roberta-base-sentiment-latest').to(device)
-    sentiment_model = torch.compile(model) # torch 2.0 feature
-    tokens = tokenizer.encode(text, return_tensors='pt').to(device)
-    result = sentiment_model(tokens)
+    encoded_input = tokenizer(text, padding=True, truncation=True)
+    #sentiment_model = torch.compile(model) torch 2.0 feature doesn'w work on windows
+    tokens = tokenizer.encode(encoded_input, return_tensors='pt').to(device)
+    result = model(tokens)
     
     return int(torch.argmax(result.logits))
 
 
 def sentiment_main(df):
+    """Applies the sentiment label function
+
+    Args:
+        df (pd.DataFrame): table containing text
+
+    Returns:
+        pd.Dataframe: same table with an additional sentiment column with associated label
+    """
     df2 = df.copy()
     df2['sentiment'] = df2['cleaned_text'].swifter.apply(lambda x: sentiment_label(x))
     df2.loc[df2["sentiment"] == 0, "sentiment"] = "Negative"
@@ -52,8 +66,12 @@ def sentiment_main(df):
     return df2
     
 
-
 def objective(trial):
+    """Bertopic is senstive to hyperparameters. Therefore function searches for best ones.
+
+    Returns:
+        float : coherence score i.e. how easy it is to interpret
+    """
     params = {
         'n_neighbors':trial.suggest_int('n_neighbors', 5, 50),
         'n_components':trial.suggest_int('n_components', 5, 50),
@@ -81,27 +99,53 @@ def objective(trial):
         [words for words, _ in topic_model.get_topic(topic) if words!=''] 
                for topic in range(len(set(topics))-1)]
 
-    coherence_model = CoherenceModel(topics=topic_words, 
-                                texts=tokens, 
-                                corpus=corpus,
-                                dictionary=dictionary, 
+    coherence_model = CoherenceModel(topics=topic_words, texts=tokens, 
+                                corpus=corpus, dictionary=dictionary, 
                                 coherence='u_mass')
     coherence = coherence_model.get_coherence()
     
     return coherence
     
-def tune_topic_model():
-    study = optuna.create_study(direction='maximize')
-    study.optimize(objective, n_trials=25)
+def tune_topic_model(tune = False):
+    """Controls wether we want to start tuning or not. Usually takes some time and 
+    can be memory intensive.
     
-    print("Best trial:")
-    trial = study.best_trial
-    print("Value: {}".format(trial.value))
-    print("Params: ")
-    for key, value in trial.params.items():
-        print("    {}: {}".format(key, value))
+    Args:
+        tune (bool, optional): start tuning. Defaults to False.
+    """
+    if tune == True:
+        study = optuna.create_study(direction='maximize')
+        study.optimize(objective, n_trials=25)
+        
+        print("Best trial:")
+        trial = study.best_trial
+        print("Value: {}".format(trial.value))
+        print("Params: ")
+        for key, value in trial.params.items():
+            print("    {}: {}".format(key, value))
+    
+def apply_topic_model():
+    umap_model = UMAP(n_neighbors= 1, n_components=1, 
+                      metric='cosine', low_memory=False)
+    hdbscan_model = HDBSCAN(min_cluster_size=1, min_samples=1,
+                            metric='euclidean', prediction_data=True)
+
+    sentence_model = SentenceTransformer("all-MiniLM-L6-v2")
+    embeddings = sentence_model.encode(docs, show_progress_bar=True)
+    topic_model = BERTopic(umap_model=umap_model, hdbscan_model=hdbscan_model,
+                           verbose=True).fit(docs, embeddings)
+    
+    topic_model.reduce_topics(docs, nr_topics=10) 
+    topics, probs = topic_model.transform(docs, embeddings)
+  
+    df = pd.DataFrame({"docs": docs, "topics": topics})
+    print(df)
+    print(topic_model.get_topic_info())
+    
+    return df
     
 if __name__ == "__main__":
     df = load_data()
     docs = df.cleaned_text.to_list()
-    tune_topic_model()
+    df_sentiment = sentiment_main(df)
+    
